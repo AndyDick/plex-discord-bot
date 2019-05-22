0b605547c30dd92753efa291db87873eb4f5a541
jadon <jadonwolffs@gmail.com>
2019-05-22 15:14:26 +0200

    Renamed template files to prevent them from affecting working

---
+++ config/keys template.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'clientId'      : 'Bot client ID',
+  'clientSecret'  : 'Bot client Secret',
+  'username'      : 'Bot username',
+  'botToken'      : 'Bot token',
+};
---
+++ config/plex template.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'hostname'    : 'Plex host',
+  'port'        : 'Plex port',
+  'username'    : 'Plex username',
+  'password'    : 'Plex password',
+  'token'       : 'Plex token',
+  'machineId'   : 'Plex machine ID',
+  'managedUser' : 'PLEX_MANAGED_USERNAME',
+  'options'     : {
+    'identifier': 'plexBot',
+    'product'   : 'plexBot',
+    'version'   : '0.9',
+    'deviceName': 'plexBot',
+    'platform'  : 'Discord',
+    'device'    : 'Discord'
+  }
+};
