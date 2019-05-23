082cb1063bd8f86538322e752826b92691d00603
jadon <jadonwolffs@gmail.com>
2019-05-23 14:42:43 +0200

    Added other commands to the core app

--- app/music.js
+++ app/music.js
@@ -1,6 +1,9 @@
 module.exports = function(client) {
   // plex commands -------------------------------------------------------------
   var plexCommands = require('../commands/plex');
+  var genCommands = require('../commands/general');
+  var botConfig = require('../config/bot');
+  var prefix = botConfig.prefix;
 
   // when bot is ready
   client.on('ready', function() {
@@ -13,14 +16,24 @@ module.exports = function(client) {
   // when message is sent to discord
   client.on('message', function(message){
     var msg = message.content.toLowerCase();
-    if (msg.startsWith('!')){
+    if (msg.startsWith(prefix)){
       var cmdTxt = msg.split(" ")[0].substring("-".length, msg.length);
       var query = msg.substring(msg.indexOf(' ')+1);
-      var cmd = plexCommands[cmdTxt];
-
-      if (cmd){
+      var pcmd = plexCommands[cmdTxt];
+      var gcmd = genCommands[cmdTxt];
+      if (pcmd){
         try {
-          cmd.process(client, message, query);
+          console.log('plex command');
+          pcmd.process(client, message, query);
+        }
+        catch (e) {
+          console.log(e);
+        }
+      }
+      else if(gcmd){
+        try {
+          console.log('general command');
+          gcmd.process(client, message, query);
         }
         catch (e) {
           console.log(e);
